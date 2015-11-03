2b34c0e1381edbe477bb8e8e978cbe2db6f6e541
bgveenstra <b.g.veenstra@gmail.com>
2015-11-03 06:47:51 -0800

    prime

--- solutions/numbers.rb
+++ solutions/numbers.rb
@@ -25,7 +25,7 @@ def is_prime?(num)
     false
   else 
     # this could be a prime! loop through and check divisibility
-    (2..num-1).each do |n|
+    (2..(num-1)).each do |n|
       if num % n == 0
         return false  # break the loop early
       end
@@ -33,6 +33,8 @@ def is_prime?(num)
     true
   end
 end
+
+
 
 def primes_less_than(num)
   primes = []
