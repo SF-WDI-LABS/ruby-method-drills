b5400fc34b1fcbf8bcda46e79c05484ec86bb694
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2016-04-22 13:28:28 -0700

    more in comments

--- drills.rb
+++ drills.rb
@@ -8,47 +8,65 @@
   # returns the input string
 
 #eddie_izzards_height
-  # calculates Eddie Izzards height (67in)
-  # and takes into account the height of his heel (default: 0in)
+  # calculates and returns Eddie Izzard's height 
+  # takes in the height of heels he's wearing (default value: 0)
+  # and adds heel height to his base 67 inches for the result
 
 #how_many_args
   # accepts any number of arguments without complaint
   # returns the total number of arguments passed to the function
+  # Hint: google ruby *args
 
-#find_answer
+#find_answer 
   # returns the value associated with the 'answer' keyword argument
   # returns nil if it cannot find the 'answer' keyword argument
   # complains when given non-keyword arguments
+  # Hint: google ruby keyword arguments
 
 ##############################
 #### MANIPULATING STRINGS ####
 ##############################
 #first_char
+  # takes in a word
   # returns the first letter of the word
   # lowercases the first letter of the word
 
 #polly_wanna
+  # takes in a word
   # echoes the original word
   # repeats the original word 3 times
+  # returns a string with the word repeated
 
 #count_chars
-  # returns the number of characters in a string
+  # takes in a word
+  # returns the number of characters in the word
 
 #yell
+  # takes in a message
   # convert the message to uppercase
   # adds an exclamation point to the end of the message
+  # returns the message
 
+## STRETCH ##
 #to_telegram
+  # takes in a message
   # replaces periods with ' STOP'
+  # returns the updated message
+
 
 #spell_out
-  # returns the input string, with characters seperated by dashes
+  # takes in a string
   # converts the string to lowercase
+  # returns the input string, with characters seperated by dashes
+
 
 #seperate
+  # takes in a string
   # seperates characters with a custom seperator, when supplied with one
   # seperates characters with dashes (by default)
+  # returns the modified string
 
+## STRETCH ##
 #croon
   # seperates word characters with dashes
   # preserves whitespace between words
@@ -56,7 +74,10 @@
 #palindrome_word?
   # determines whether a single word is a palindrome
   # ignores case
+  # returns true or false
 
+
+## SUPER STRETCH ##
 #palindrome_sentence?
   # determines whether a sentence is a palindrome
   # ignores case
@@ -64,51 +85,66 @@
   # ignores punctuation
 
 #is_vowel
-  # determines whether a given character is a vowel
+  # takes in a string of one character
+  # determines whether the character is a vowel
   # ignores case
   # handles weird inputs gracefully
 
 #add_period
+  # takes in a string
   # adds a period to the end of the sentence
   # does not add a period if one is already there
   # does not add a period if any form of terminal punctuation is present
+  # returns the sentence
 
 
 ###########################
 #### LOOPS & ITERATORS ####
 ###########################
 #count_spaces
+  # takes in a string
   # counts the spaces in a string
+  # returns number of spaces
 
 #string_lengths
-  # converts a list of strings to a list of string lengths
+  # takes in an array of strings
+  # returns an array containing the lengths of the strings
 
 #remove_falsy_values
+  # takes in a list
   # filters out falsy values from a list
+  # returns the updated list
 
 #exclude_last
-  # removes the last item from an array
-  # removes the last item from a string
+  # takes in an array or string
+  # removes the last item from the array or string
+  # returns it
 
 #exclude_first
+  # takes in an array or string
   # removes the first item from an array
   # removes the first character from a string
-  # does not alter the original input (non-destructive)
+  # returns a new string - does not alter the original input (non-destructive)
 
 #exclude_ends
+  # takes in an array or string
   # removes the first and last items from an array
   # removes the first and last characters from a string
 
 #select_every_even
-  # returns a list of even-indexed items
+  # takes in an array
+  # returns a list of even-indexed items from the input
 
 #select_every_odd
+  # takes in an array
   # returns a list of odd-indexed items
 
 #select_every_n
+  # takes in an array
   # returns a list of items at an index evenly divisible by n
   # defaults to an n value of 1
 
+## STRETCH ##
 #compile_agenda
   # converts a list of agenda items into a single string
   # titles start with a bullet ("*") and are separated by line breaks ("/n")
@@ -120,10 +156,13 @@
 #### MANIPULATING NUMBERS ####
 ##############################
 #count_to
+  # takes in a number
   # returns an array containing every integer from 0 to n
+  # counts up or down
   # rounds off decimals
 
 #is_integer?
+  # takes in a number
   # returns true for Fixnums and Bignums (whole number or 'integer' types)
   # returns true for Floats (decimals) equal to integers
   # returns false for non-integer decimals
@@ -131,21 +170,26 @@
   # returns false for non-numbers
 
 #is_prime?
+  # takes in a number and checks if it's prime
   # returns false for non-integer decimals
   # returns false for numbers less than or equal to 1
   # returns false for numbers divisible by anything but 1 and themselves
   # returns true for prime numbers
+  # Hint: google prime numbers!
 
 #primes_less_than
+  # takes in a number
   # returns an empty array if there are no primes below num
   # does not return the number itself
   # finds all primes less than the given number
 
+## STRETCH ##
 #iterative_factorial
+  # takes in a number
   # returns 1 for 0 and 1
   # returns NaN for numbers less than 0
   # returns NaN for non-integers
-  # calculates factorial
+  # calculates and returns the factorial of the input number
 
 
 
@@ -153,13 +197,20 @@
 #### MANIPULATING OBJECTS ####
 ##############################
 #character_count
+  # takes in a string
   # counts how many times each character appears in a string
   # ignores case
+  # returns the hash
 
+## STRETCH ##
 #word_count
+  # takes in a string
   # counts how many times a word appears in a string
   # ignores case
   # ignores characters that are not in the sequence a-z
+  # returns a hash with all the words and their counts
 
+## STRETCH ##
 #most_frequent_word
+  # takes in a string
   # finds the word in a string that appears with the most frequency
