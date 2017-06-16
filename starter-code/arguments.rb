98d60d06a21c9b156097f11a0fa85c9d19fec5c4
younjiwoo <younjiwoo@gmail.com>
2017-06-16 15:47:29 -0700

    finished eddie_izzards_height method

--- starter-code/arguments.rb
+++ starter-code/arguments.rb
@@ -3,15 +3,24 @@
 #########################
 #say_hello
   # returns 'hello'
+def say_hello
+	'hello'
+end 
 
 #echo
   # returns the input string
   # i.e., if the passed input string is "hello", returns "hello"
+def echo(str)
+	str
+end
 
 #eddie_izzards_height
   # calculates and returns Eddie Izzard's height
   # takes in the height of heels he's wearing (default value: 0)
   # and adds heel height to his base 67 inches for the result
+def eddie_izzards_height(heel_height = 0)
+	67 + heel_height
+end 
 
 #how_many_args
   # accepts any number of arguments without complaint
