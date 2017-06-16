1330fdb27ae5986188b8217babd8fea9124134ec
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-06-16 15:49:17 -0700

    passed 2 tests for argument_spec

--- starter-code/arguments.rb
+++ starter-code/arguments.rb
@@ -3,10 +3,17 @@
 #########################
 #say_hello
   # returns 'hello'
+  def say_hello
+   "hello"
+  end
 
 #echo
   # returns the input string
   # i.e., if the passed input string is "hello", returns "hello"
+  def echo(word)
+    word
+  end
+
 
 #eddie_izzards_height
   # calculates and returns Eddie Izzard's height
