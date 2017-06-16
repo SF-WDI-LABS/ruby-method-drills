adb5f88bb73b9312a66209796fa8bf49476c33a7
younjiwoo <younjiwoo@gmail.com>
2017-06-16 16:25:10 -0700

    finished arguments.rb

--- starter-code/arguments.rb
+++ starter-code/arguments.rb
@@ -35,3 +35,7 @@ end
   # returns nil if it cannot find the 'answer' keyword argument
   # complains when given non-keyword arguments
   # Hint: google ruby keyword arguments
+def find_answer(options={})
+	options[:answer]
+end 
+
