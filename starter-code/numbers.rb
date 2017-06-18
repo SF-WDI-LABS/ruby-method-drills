322b9bb2805df941ec95700298c176f3b0e6c71f
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-06-17 18:49:07 -0700

    finished is_integer? and is_prime?

--- starter-code/numbers.rb
+++ starter-code/numbers.rb
@@ -17,6 +17,13 @@ end
   # rounds off decimals
 
 #is_integer?
+def is_integer? num
+  if num.is_a?(Numeric)
+    num.is_a?(Fixnum) || num.is_a?(Bignum) || (num.is_a?(Float) && num.to_i == num && !num.nan?)
+  else
+    false
+  end
+end
   # takes in a number
   # returns true for Fixnums and Bignums (whole number or 'integer' types)
   # returns true for Floats (decimals) equal to integers
@@ -25,6 +32,21 @@ end
   # returns false for non-numbers
 
 #is_prime?
+def is_prime? num
+  if num.is_a?(Float) || num <= 1
+    false
+  else
+    ret = true
+    2.step(num-1,1) { |i|
+      # puts i
+      # puts num % i === 0
+      if num % i === 0
+        ret = false
+      end
+    }
+    ret
+  end
+end
   # takes in a number and checks if it's prime
   # returns false for non-integer decimals
   # returns false for numbers less than or equal to 1
