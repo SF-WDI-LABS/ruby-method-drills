b411fd56257d44acef20a95215a0f69a5fa53b7e
drstrangele <chickenle@gmail.com>
2017-06-18 11:36:56 -0700

    Completed arguments

--- starter-code/arguments.rb
+++ starter-code/arguments.rb
@@ -3,23 +3,39 @@
 #########################
 #say_hello
   # returns 'hello'
+  def say_hello
+    'hello'
+  end
 
 #echo
   # returns the input string
   # i.e., if the passed input string is "hello", returns "hello"
+  def echo input
+    input
+  end
 
 #eddie_izzards_height
   # calculates and returns Eddie Izzard's height
   # takes in the height of heels he's wearing (default value: 0)
   # and adds heel height to his base 67 inches for the result
+  def eddie_izzards_height heel_height=0
+    67 + heel_height
+  end
+
 
 #how_many_args
   # accepts any number of arguments without complaint
   # returns the total number of arguments passed to the function
   # Hint: google ruby *args
+  def how_many_args *args
+    args.length
+  end
 
 #find_answer
   # returns the value associated with the 'answer' keyword argument
   # returns nil if it cannot find the 'answer' keyword argument
   # complains when given non-keyword arguments
   # Hint: google ruby keyword arguments
+  def find_answer word={}
+    word[:answer]
+  end
