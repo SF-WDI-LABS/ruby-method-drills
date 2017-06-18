54ba18b0fccddbbf98e285e7e46cabf7a9942a83
kat tsukuda <bearheartsbat@gmail.com>
2017-06-18 08:21:26 -0700

    pass all exercises for numbers.rb

--- starter-code/numbers.rb
+++ starter-code/numbers.rb
@@ -18,7 +18,7 @@ end
 #is_integer?
 def is_integer? n
   n.class == Fixnum || n.class == Bignum ||
-    n.is_a?(Float) && !n.nan? && n.to_i == n 
+    n.is_a?(Float) && !n.nan? && n.to_i == n
 end
   # takes in a number
   # returns true for Fixnums and Bignums (whole number or 'integer' types)
@@ -28,6 +28,18 @@ end
   # returns false for non-numbers
 
 #is_prime?
+def is_prime? num
+  if !is_integer?(num) || num <= 1
+    false
+  else
+    (2..(num-1)).each do |n|
+      if num % n == 0
+        return false
+      end
+    end
+    true
+  end
+end
   # takes in a number and checks if it's prime
   # returns false for non-integer decimals
   # returns false for numbers less than or equal to 1
@@ -36,6 +48,15 @@ end
   # Hint: google prime numbers!
 
 #primes_less_than
+def primes_less_than(num)
+  primes = []
+  (2..num-1).each do |n|
+    if is_prime?(n)
+      primes.push n
+    end
+  end
+  primes
+end
   # takes in a number
   # returns an empty array if there are no primes below num
   # does not return the number itself
@@ -43,6 +64,16 @@ end
 
 ## STRETCH ##
 #iterative_factorial
+def iterative_factorial(num)
+  if num < 0 || !is_integer?(num)
+    return Float::NAN
+  end
+  potato = 1
+  (1..num).each do |i|
+    potato = potato*i
+  end
+  potato
+end
   # takes in a number
   # returns 1 for 0 and 1
   # returns NaN for numbers less than 0
