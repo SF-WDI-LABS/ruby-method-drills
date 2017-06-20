ec4e69e6856434f5b4525e7a0466ca5934daec74
kat tsukuda <bearheartsbat@gmail.com>
2017-06-19 21:13:25 -0700

    pass all for hashes.rb

--- starter-code/hashes.rb
+++ starter-code/hashes.rb
@@ -2,6 +2,18 @@
 #### MANIPULATING HASHES ####
 ##############################
 #character_count
+def character_count string
+  char_count = {}
+  string.each_char do |char|
+    char = char.downcase
+    if char_count[char].nil?
+      char_count[char] = 1
+    else
+      char_count[char] += 1
+    end
+  end
+  char_count
+end
   # takes in a string
   # counts how many times each character appears in a string
   # ignores case
@@ -9,6 +21,18 @@
 
 ## STRETCH ##
 #word_count
+def word_count string
+  word_counts = {}
+  string.split(" ").each do |word|
+    word = word.downcase.gsub(/[^a-z]/i, "")
+    if word_counts[word].nil?
+      word_counts[word] = 1
+    else
+      word_counts[word] += 1
+    end
+  end
+  word_counts
+end
   # takes in a string
   # counts how many times a word appears in a string
   # ignores case
@@ -17,5 +41,9 @@
 
 ## STRETCH ##
 #most_frequent_word
+def most_frequent_word string
+  word_count = word_count string
+  word_count.empty? ? nil : word_count.invert.max[1]
+end
   # takes in a string
   # finds the word in a string that appears with the most frequency
