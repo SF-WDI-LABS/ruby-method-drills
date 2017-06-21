c4a31e4992806cdf0d44d1c34de4d6e9abd5aba9
drstrangele <chickenle@gmail.com>
2017-06-20 21:05:41 -0700

    finished character_count

--- starter-code/hashes.rb
+++ starter-code/hashes.rb
@@ -6,9 +6,23 @@
   # counts how many times each character appears in a string
   # ignores case
   # returns the hash
-  def character_count  potato
-    potato
-  end
+
+def character_count(stringthing)
+  letter_count = {}
+  last_char = nil
+  char_one_two_three = 0
+  stringthing.each_char do |char|
+  char = char.downcase
+    if char != last_char
+      char_one_two_three = 1
+    else
+      char_one_two_three += 1
+    end
+    last_char = char
+    letter_count[char] = char_one_two_three if (letter_count[char]||0) < char_one_two_three
+end
+letter_count
+end
 
 ## STRETCH ##
 #word_count
